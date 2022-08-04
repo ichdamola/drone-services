d82ca808e2ebd97e874ae99c4d683e64bd8ab2c1
Ichdamola <adedadamola@gmail.com>
2022-08-04 08:42:39 +0100

    [UPD] install the drone app

--- configurations/settings.py
+++ configurations/settings.py
@@ -42,7 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party apps
     'drf_yasg',
+
+    # Local
+    "drones",
 ]
 
 MIDDLEWARE = [
